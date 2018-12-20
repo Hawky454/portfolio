945cf442d2e1d387d48b41d1353d2bd3f8eefa9a
David Miller <hawky454@gmail.com>
2018-12-20 16:33:02 -0600

    added alert for sent messages

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -9,7 +9,7 @@ function getHostURL() {
   if (window.location.host.indexOf('localhost') !== -1) {
     return 'http://localhost:8000';
   } else {
-    return 'https://dmiller-portfolio-server.herokuapp.com'; //!this address needs to be changed... need to deploy server first...
+    return 'https://dmiller-portfolio-server.herokuapp.com';
   }
 }
 
@@ -50,7 +50,7 @@ class Contact extends Component {
       comment: ''
     });
     alert(
-      "Thank you for contacting me, I'll get back with you as soon as I can"
+      "Thank you for contacting me, I'll get back with you as soon as I can."
     );
   };
 
