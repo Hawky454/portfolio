bbbf1e971e94d1b94691ad7f2f9e47506f885c1f
David Miller <hawky454@gmail.com>
2019-06-21 00:35:50 -0500

    added dynamic time and style changes still need to figure out how to get the hours and minutes to lead with zeros

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -8,8 +8,36 @@ const ContactForm = props => {
   const reason = props.reason;
   const message = props.comment;
 
+  const timeStyle = {
+    fontSize: '25px',
+    boxShadow: '1px 2.5px 5px',
+    padding: '20px'
+  }
+
+  const date = new Date();
+  let hours = date.getHours();
+  const minutes = date.getMinutes();
+
+  let timeOfDay;
+
+  if (hours < 12) {
+    timeOfDay = 'morning';
+    timeStyle.color = '#04756F'
+  } else if (hours >= 12 && hours < 17) {
+    timeOfDay = 'afternoon';
+    timeStyle.color = '#8914A3'
+  } else {
+    timeOfDay = 'night';
+    timeStyle.color = '#D90000'
+  }
+
+
+
+
   return (
+
     <div className="form-container">
+      <p style={timeStyle}>{hours % 12}:{minutes} Good {timeOfDay}!</p>
       <p className="contact-form-info">David, </p>
       <p className="contact-form-info">
         I can be reached at:
